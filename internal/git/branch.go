c51125c74a747987c823d5ed2f3c6242a9c9e208
Camden DeVoogd <camdendevoogd@gmail.com>
2022-07-16 09:09:19 -0700

    Determine branch type using prefixes
    When the `git branch` command is run, some branches have a prefix that
    signal what kind of branch they are. The branch prefixed with `*` is the
    branch that is currently checked out, while the `+` prefix is a branch
    that is checked out in a Git worktree.
    
    I'm using these prefixes to determine the branch type and then removing
    them from the name. The type can be used later on to change styling when
    showing to the user, since it is important for the user to know the
    various branch types.

--- internal/git/branch.go
+++ internal/git/branch.go
@@ -3,6 +3,8 @@ package git
 import (
 	"os/exec"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type BranchType int
@@ -18,14 +20,54 @@ type Branch struct {
 	Name string
 }
 
+// NewBranch constructs a new Branch structure based on the given branch. The branch name must
+// match the name of a branch that exists in the current Git repository. Using the name, the
+// function will gather information about the branch and populate the structure.
+func NewBranch(name string) (*Branch, error) {
+	branchType, strippedName := determineBranchType(name)
+	return &Branch{Type: branchType, Name: strippedName}, nil
+}
+
 // Branches returns a list of Git branches in the current repository.
-func Branches() ([]string, error) {
+func Branches() ([]*Branch, error) {
 	cmd := exec.Command("git", "branch", "--list")
 	stdout, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
+	output := strings.TrimSpace(string(stdout))
 
-	branches := strings.Split(string(stdout), "\n")
+	var branches []*Branch
+	for _, branch := range strings.Split(output, "\n") {
+		b, err := NewBranch(branch)
+		if err != nil {
+			return nil, errors.Wrap(err, "error constructing branch")
+		}
+		branches = append(branches, b)
+	}
+
 	return branches, nil
+}
+
+const (
+	worktreeBranchPrefix = "+"
+	currentBranchPrefix  = "*"
+)
+
+// determineBranchType returns the type of the branch using the prefixes in its name as returned by
+// the `git branch` command. The function also returns the name back to the caller with all
+// prefixes and whitespace trimmed.
+func determineBranchType(name string) (branchType BranchType, strippedName string) {
+	switch {
+	case strings.HasPrefix(name, currentBranchPrefix):
+		branchType = BranchTypeCurrent
+		name = strings.TrimPrefix(name, currentBranchPrefix)
+	case strings.HasPrefix(name, worktreeBranchPrefix):
+		branchType = BranchTypeWorktree
+		name = strings.TrimPrefix(name, worktreeBranchPrefix)
+	default:
+		branchType = BranchTypeNormal
+	}
+
+	return branchType, strings.TrimSpace(name)
 }
