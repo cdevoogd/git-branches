323f1b16e6d7e08719a649d081d3a2c69a3b81be
Camden DeVoogd <camdendevoogd@gmail.com>
2023-10-20 23:04:40 -0600

    Integrate delete command with new TUI

--- internal/commands/delete/delete.go
+++ internal/commands/delete/delete.go
@@ -1,13 +1,10 @@
 package delete
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/cdevoogd/git-branches/internal/git"
-	"github.com/cqroot/prompt"
-	"github.com/cqroot/prompt/multichoose"
+	"github.com/cdevoogd/git-branches/internal/tui"
 	"github.com/spf13/cobra"
 )
 
@@ -24,79 +21,33 @@ func Execute(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("error loading branches: %w", err)
 	}
 
-	handler := newDeleteHandler(branches)
-	toDelete, err := handler.getBranchesToDelete()
-	if err != nil {
-		if errors.Is(err, prompt.ErrUserQuit) {
-			return nil
+	choices := make([]*tui.Choice, len(branches))
+	for i, branch := range branches {
+		choices[i], err = tui.NewChoiceFromBranch(branch)
+		if err != nil {
+			return fmt.Errorf("error converting branch to choice: %w", err)
 		}
+	}
+
+	selections, err := tui.PromptForSelections(choices)
+	if err != nil {
 		return err
 	}
 
-	if len(toDelete) == 0 {
+	if len(selections) == 0 {
 		fmt.Println("No branches were selected")
 		return nil
 	}
 
-	err = git.DeleteBranches(toDelete)
+	branchNames := make([]string, len(selections))
+	for i, selection := range selections {
+		branchNames[i] = selection.Name
+	}
+
+	err = git.DeleteBranches(branchNames)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-type deleteHandler struct {
-	choices  []string
-	branches map[string]*git.Branch
-}
-
-func newDeleteHandler(branches []*git.Branch) *deleteHandler {
-	handler := &deleteHandler{
-		choices:  make([]string, len(branches)),
-		branches: make(map[string]*git.Branch),
-	}
-
-	for i, branch := range branches {
-		handler.choices[i] = branch.Name
-		handler.branches[branch.Name] = branch
-	}
-
-	return handler
-}
-
-func (d *deleteHandler) getBranchesToDelete() ([]string, error) {
-	msg := "Choose branches to delete:"
-	return prompt.New(prompt.WithTheme(themePrompt)).Ask(msg).MultiChoose(
-		d.choices,
-		multichoose.WithTheme(d.themeChoices),
-		multichoose.WithHelp(true),
-	)
-}
-
-// themeChoices returns a styled string that can be used to display the branch choices to the user.
-// It is meant to fulfil the multichoose.Theme interface.
-func (d *deleteHandler) themeChoices(branches []string, cursor int, isSelected multichoose.IsSelected) string {
-	s := strings.Builder{}
-	s.WriteString("\n")
-
-	for i, name := range branches {
-		symbol, style := getDisplayStyle(i == cursor, isSelected(i))
-		s.WriteString(style.Render(fmt.Sprintf("[%s] %s", symbol, name)))
-
-		branch, ok := d.branches[name]
-		if ok {
-			if branch.Type != git.BranchTypeNormal {
-				s.WriteString(style.Render(fmt.Sprintf(" (%s)", branch.Type.String())))
-			}
-
-			if branch.Description != "" {
-				s.WriteString(style.Render(fmt.Sprintf(" (%s)", branch.Description)))
-			}
-		}
-
-		s.WriteString("\n")
-	}
-
-	return s.String()
 }
